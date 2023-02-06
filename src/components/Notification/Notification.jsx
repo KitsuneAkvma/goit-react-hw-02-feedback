97e5797551560c7edf06479b100fa15b7b705a7e
KitsuneAkvma <106407746+KitsuneAkvma@users.noreply.github.com>
2023-02-06 21:55:17 +0000

    Deploying to gh-pages from @ KitsuneAkvma/goit-react-hw-02-feedback@6d2a494b04663c657eabfcd2ab82dd1761ab37b9 🚀

--- src/components/Notification/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { StyledNotification } from './Notification.styled';
-
-export function Notification() {
-  return <StyledNotification>No feedback been given</StyledNotification>;
-}
